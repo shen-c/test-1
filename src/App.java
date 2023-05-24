900e1fec932ae3a9615870ebff0274e8f58dd98f
shen-c <tobias.shen@gmail.com>
2023-05-23 17:45:33 -0700

    test

--- src/App.java
+++ src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Wworldddddd!");
+        System.out.println("Hello, Wworld!");
+
+        System.out.println("beans");
     }
 }
