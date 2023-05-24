94ad6da0d7af7f91af69a6e6e9dacd97e51f55b4
shen-c <113067599+shen-c@users.noreply.github.com>
2023-05-23 17:46:11 -0700

    Update App.java

--- src/App.java
+++ src/App.java
@@ -2,6 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, Wworld!");
 
-        System.out.println("beans");
+        System.out.println("ducks");
     }
 }
