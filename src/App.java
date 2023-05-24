0ad1408b8e86137cf94df9179447a1e41abef9de
shen-c <113067599+shen-c@users.noreply.github.com>
2023-05-23 17:42:37 -0700

    Update App.java
    added some D's to hello world

--- src/App.java
+++ src/App.java
@@ -1,5 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, Wworld!");
+        System.out.println("Hello, Wworldddddd!");
     }
 }
